a5596410702edd0e8b1fe55a7f0b80adb82125e3
vaskevichk <kate.panasiuk@yahoo.com>
2023-11-16 11:50:53 +0200

    blueprint

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { QuizAttempt } from 'src/quiz_attempts/entities/quiz_attempt.entity'
 import { Quiz } from 'src/quizzes/entities/quiz.entity'
@@ -8,14 +8,53 @@ import { Repository } from 'typeorm'
 @Injectable()
 export class AnalyticsService {
   constructor(
-    @InjectRepository(Quiz)
-    private readonly quizRepository: Repository<Quiz>,
+    // @InjectRepository(Quiz)
+    // private readonly quizRepository: Repository<Quiz>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    @InjectRepository(QuizAttempt)
-    private readonly quizAttemptRepository: Repository<QuizAttempt>,
+    // @InjectRepository(QuizAttempt)
+    // private readonly quizAttemptRepository: Repository<QuizAttempt>,
   ) {}
+
+  // get user's avarage rating of all quiz attempts in the system
   async getUserAverage(userId: number) {
+    try {
+      const user = await this.userRepository.findOne({
+        where: { id: userId },
+        relations: [
+          'quizAttempts',
+          'quizAttempts.quiz',
+          'quizAttempts.quiz.company',
+        ],
+      })
+
+      if (!user) {
+        throw new NotFoundException('User is not found')
+      }
+
+      let totalRating = 0
+      let totalAttempts = 0
+
+      // Iterate through the user's quiz attempts
+      user.quizAttempts.forEach((attempt) => {
+        totalRating += attempt.overallRatingAcrossSystem
+        totalAttempts++
+      })
+
+      // Calculate the average rating
+      const averageRating = totalAttempts > 0 ? totalRating / totalAttempts : 0
+      console.log(averageRating, "averageRating")
+      return {
+        userId: user.id,
+        averageRating: averageRating.toFixed(0), 
+      }
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  // get list of the avarage rating particular quiz attempt in all companies
+  async getUserQuizAverages(userId: number, quizId: number) {
     const user = await this.userRepository.findOne({
       where: { id: userId },
       relations: [
@@ -30,22 +69,7 @@ export class AnalyticsService {
     }
   }
 
-  async getUserAllAverage(userId: number) {
-    const user = await this.userRepository.findOne({
-      where: { id: userId },
-      relations: [
-        'quizAttempts',
-        'quizAttempts.quiz',
-        'quizAttempts.quiz.company',
-      ],
-    })
-
-    if (!user) {
-      throw new NotFoundException('User is not found')
-    }
-  }
-
-  async getUserAllQuizAnalysis(userId: number) {
+  async getUserAllQuizAverages(userId: number) {
     const user = await this.userRepository.findOne({
       where: { id: userId },
       relations: [
